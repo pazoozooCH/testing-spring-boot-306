5d3a1d6c3cb2a2571d9344dbeef6f10cc0ce32c9
Martin Meyer <martin.meyer.1@post.ch>
2023-04-21 10:50:01 +0200

    Initial commit
    - Created with Spring Initializr

---
+++ src/main/java/ch/inftec/springboot306/SpringBoot306Application.java
@@ -0,0 +1,13 @@
+package ch.inftec.springboot306;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot306Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot306Application.class, args);
+	}
+
+}
---
+++ src/test/java/ch/inftec/springboot306/SpringBoot306ApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.inftec.springboot306;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot306ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
